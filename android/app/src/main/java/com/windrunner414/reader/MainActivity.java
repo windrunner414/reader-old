b1d97961afd19b6f730f26171b7c746041487d04
windrunner414 <windrunner414@outlook.com>
2019-02-24 18:50:53 +0800

    update

--- android/app/src/main/java/com/windrunner414/reader/MainActivity.java
+++ android/app/src/main/java/com/windrunner414/reader/MainActivity.java
@@ -1,6 +1,7 @@
 package com.windrunner414.reader;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 import io.flutter.app.FlutterActivity;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
@@ -9,5 +10,9 @@ public class MainActivity extends FlutterActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     GeneratedPluginRegistrant.registerWith(this);
+
+    WindowManager.LayoutParams lp = getWindow().getAttributes();
+    lp.layoutInDisplayCutoutMode = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES;
+    getWindow().setAttributes(lp);
   }
 }
